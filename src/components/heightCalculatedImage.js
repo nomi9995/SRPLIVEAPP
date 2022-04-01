c63d82091683df9958ab0099ed24ebe5c91aa358
saeedahmed2023 <87636516+saeedahmed2023@users.noreply.github.com>
2022-04-01 16:37:51 +0500

    message bubble video static height width

--- src/components/heightCalculatedImage.js
+++ src/components/heightCalculatedImage.js
@@ -1,10 +1,40 @@
-import React, {useState} from 'react';
-import {Text, TextInput, View, Dimensions} from 'react-native';
-import FastImage from 'react-native-fast-image';
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-const PizzaTranslator = props => {
+import React, { useState } from "react";
+import { Text, TextInput, View, Dimensions } from "react-native";
+import FastImage from "react-native-fast-image";
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+const PizzaTranslator = (props) => {
   const [calcImgHeight, setcalcImgHeight] = useState(0);
+  const ImageCalculateHeight = (img) => {
+    try {
+      let heightWidth = img.slice(
+        img.lastIndexOf("_") + 1,
+        img.lastIndexOf(".")
+      );
+      // console.log('img', heightWidth.slice(0, heightWidth.indexOf('x')));
+      let data = parseInt(
+        heightWidth.slice(heightWidth.indexOf("x") + 1, heightWidth.length)
+      );
+      return data == "NaN" ? 100 : data;
+    } catch (e) {
+      console.log("error", img, e);
+      return 20;
+    }
+  };
+  const ImageCalculateWidth = (img) => {
+    try {
+      let heightWidth = img.slice(
+        img.lastIndexOf("_") + 1,
+        img.lastIndexOf(".")
+      );
+      let data = parseInt(heightWidth.slice(0, heightWidth.indexOf("x")));
+
+      return data == "NaN" ? 100 : data;
+    } catch (e) {
+      console.log("error", img, e);
+      return 20;
+    }
+  };
   return (
     <View>
       {/* <Text style={{padding: 10, fontSize: 42}}>dsddd</Text> */}
@@ -13,18 +43,20 @@ const PizzaTranslator = props => {
           uri: props.uri,
         }}
         style={{
-          width: windowWidth * 0.7,
-          height: calcImgHeight,
+          width: windowWidth * 0.85,
+          height:
+            (ImageCalculateHeight(props.uri.replace(/ /g, "%20")) /
+              ImageCalculateWidth(props.uri.replace(/ /g, "%20"))) *
+            windowWidth *
+            0.7,
           borderRadius: 10,
         }}
-        onLoad={evt =>
+        onLoad={(evt) =>
           setcalcImgHeight(
-            (evt.nativeEvent.height / evt.nativeEvent.width) *
-              windowWidth *
-              0.6,
+            (evt.nativeEvent.height / evt.nativeEvent.width) * windowWidth * 0.6
           )
         }
-        resizeMode={'stretch'}
+        resizeMode={"stretch"}
       />
     </View>
   );
