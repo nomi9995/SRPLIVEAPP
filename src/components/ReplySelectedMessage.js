e5eca20d28ac57653775e65ce95f6ba8af951e33
Haider-Ali-7 <haideralitariqcheema@gmail.com>
2022-04-06 17:23:22 +0500

    Fixed ReplySelected ui, not found comp

--- src/components/ReplySelectedMessage.js
+++ src/components/ReplySelectedMessage.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from "react";
 import {
   View,
   Text,
   TouchableOpacity,
   StyleSheet,
   BackHandler,
-} from 'react-native';
-import FastImage from 'react-native-fast-image';
-import FontAwesome from 'react-native-vector-icons/dist/FontAwesome5';
+} from "react-native";
+import FastImage from "react-native-fast-image";
+import FontAwesome from "react-native-vector-icons/dist/FontAwesome5";
 
 //Redux
-import { setOnLongPress, setReplyState, setMessageEdit, setMessageText } from '../store/actions';
-import { connect } from 'react-redux';
+import {
+  setOnLongPress,
+  setReplyState,
+  setMessageEdit,
+  setMessageText,
+} from "../store/actions";
+import { connect } from "react-redux";
 
 class ReplySelectedMessage extends React.PureComponent {
   constructor(props) {
@@ -20,22 +25,22 @@ class ReplySelectedMessage extends React.PureComponent {
   }
 
   componentDidMount = () => {
-    BackHandler.addEventListener('hardwareBackPress', this.backHandler)
-  }
+    BackHandler.addEventListener("hardwareBackPress", this.backHandler);
+  };
 
   componentWillUnmount = () => {
-    BackHandler.removeEventListener('hardwareBackPress', this.backHandler)
-  }
+    BackHandler.removeEventListener("hardwareBackPress", this.backHandler);
+  };
 
   backHandler = () => {
-    this.onPressCross()
-    return true
-  }
+    this.onPressCross();
+    return true;
+  };
 
   onPressCross = () => {
     this.props.onSetReplyState(false);
-    this.props.onSetMessageEdit(false)
-    this.props.onSetMessageText(null)
+    this.props.onSetMessageEdit(false);
+    this.props.onSetMessageText(null);
     this.props.onSetOnLongPress([]);
   };
 
@@ -45,22 +50,26 @@ class ReplySelectedMessage extends React.PureComponent {
       <View style={styles.container}>
         <View style={styles.mainWrapper}>
           <View style={styles.leftWrapper}>
-            <Text style={styles.nameText}>{longPress[0]?.user._id === 2 ? 'You' : longPress[0]?.first_name}</Text>
+            <Text style={styles.nameText}>
+              {longPress[0]?.user._id === 2 ? "You" : longPress[0]?.first_name}
+            </Text>
 
             {longPress[0]?.type === 1 && (
-              <Text style={styles.plainMsgText} numberOfLines={1}>{longPress[0]?.message}</Text>
+              <Text style={styles.plainMsgText} numberOfLines={1}>
+                {longPress[0]?.message}
+              </Text>
             )}
 
             {longPress[0]?.type === 2 && (
               <View style={styles.mediaMsgWrapper}>
-                <FontAwesome name={'image'} size={20} color={'gray'} />
+                <FontAwesome name={"image"} size={20} color={"gray"} />
                 <Text style={styles.mediaMsgText}>Photo</Text>
               </View>
             )}
-            
+
             {longPress[0]?.type === 3 && (
               <View style={styles.mediaMsgWrapper}>
-                <FontAwesome name={'image'} size={20} color={'gray'} />
+                <FontAwesome name={"image"} size={20} color={"gray"} />
                 <Text style={styles.mediaMsgText}>GIF</Text>
               </View>
             )}
@@ -70,40 +79,43 @@ class ReplySelectedMessage extends React.PureComponent {
                 source={{
                   uri: `https://www.srplivehelp.com/media/stickers/${longPress[0]?.message}`,
                 }}
-                style={{paddingVertical: 5, height: 40, width: 40}}
+                style={{ paddingVertical: 5, height: 40, width: 40 }}
               />
             )}
 
             {longPress[0]?.type === 5 && (
               <View style={styles.mediaMsgWrapper}>
-                <FontAwesome name={'link'} size={20} color={'gray'} />
+                <FontAwesome name={"link"} size={20} color={"gray"} />
                 <Text style={styles.mediaMsgText}>Link</Text>
               </View>
             )}
 
             {longPress[0]?.type === 6 && (
               <View style={styles.mediaMsgWrapper}>
-                <FontAwesome name={'file'} size={20} color={'gray'} />
+                <FontAwesome name={"file"} size={20} color={"gray"} />
                 <Text style={styles.mediaMsgText}>File</Text>
               </View>
             )}
 
             {longPress[0]?.type === 7 && (
               <View style={styles.mediaMsgWrapper}>
-                <FontAwesome name={'headphones'} size={20} color={'gray'} />
+                <FontAwesome name={"headphones"} size={20} color={"gray"} />
                 <Text style={styles.mediaMsgText}>Audio</Text>
               </View>
             )}
 
             {longPress[0]?.type === 11 && (
               <View style={styles.mediaMsgWrapper}>
-                <FontAwesome name={'video'} size={20} color={'gray'} />
+                <FontAwesome name={"video"} size={20} color={"gray"} />
                 <Text style={styles.mediaMsgText}>Video</Text>
               </View>
             )}
           </View>
-          <TouchableOpacity style={styles.closeBtnWraper} onPress={() => this.onPressCross()}>
-            <FontAwesome name={'times'} size={20} color="lightgrey" />
+          <TouchableOpacity
+            style={styles.closeBtnWraper}
+            onPress={() => this.onPressCross()}
+          >
+            <FontAwesome name={"times"} size={20} color="lightgrey" />
           </TouchableOpacity>
         </View>
       </View>
@@ -114,17 +126,18 @@ class ReplySelectedMessage extends React.PureComponent {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f9f9f9',
+    backgroundColor: "#fff",
     paddingHorizontal: 15,
+    paddingBottom: 1,
   },
 
   mainWrapper: {
     flex: 1,
-    backgroundColor: '#fff',
-    flexDirection: 'row',
+    backgroundColor: "#f9f9f9",
+    flexDirection: "row",
     paddingLeft: 20,
     borderRadius: 12,
-    marginTop: 10
+    marginTop: 10,
   },
 
   leftWrapper: {
@@ -133,48 +146,48 @@ const styles = StyleSheet.create({
 
   nameText: {
     paddingVertical: 5,
-    color: '#057e6f'
+    color: "#057e6f",
   },
 
   plainMsgText: {
-    paddingVertical: 5
+    paddingVertical: 5,
   },
 
   mediaMsgWrapper: {
     paddingVertical: 5,
-    flexDirection: 'row'
+    flexDirection: "row",
   },
 
   mediaMsgText: {
-    marginLeft: 5
+    marginLeft: 5,
   },
 
   closeBtnWraper: {
     flex: 0.1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     longPress: state.messages.longPress,
     replyState: state.stateHandler.replyState,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    onSetOnLongPress: data => {
+    onSetOnLongPress: (data) => {
       dispatch(setOnLongPress(data));
     },
-    onSetReplyState: data => {
+    onSetReplyState: (data) => {
       dispatch(setReplyState(data));
     },
-    onSetMessageEdit: data => {
+    onSetMessageEdit: (data) => {
       dispatch(setMessageEdit(data));
     },
-    onSetMessageText: text => {
+    onSetMessageText: (text) => {
       dispatch(setMessageText(text));
     },
   };
@@ -182,5 +195,5 @@ const mapDispatchToProps = dispatch => {
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
+  mapDispatchToProps
 )(ReplySelectedMessage);
